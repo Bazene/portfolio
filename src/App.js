d27c44768c2bcd552ccff21f239335c599df6926
bazene <bazenesergeamos0@gmail.com>
2024-10-16 18:50:00 +0100

    Add Intro content : animated text and texts

--- src/App.js
+++ src/App.js
@@ -62,8 +62,21 @@ function App() {
       </div>
 
       {/* Animated text */}
+      <div className='absolute top-1/3 left-5 text-xl sm:left-10 sm:text-4xl md:lelt-1/4 md:text-6xl lg:left-5 xl:left-48 xl:text-7xl font-bold'>
+        <span className='text-gray-600'>Frelance</span>
+        <p className='text-red-500'>Developer</p>
+      </div>
+
       {/* texts */}
-      <div></div>
+      <div className='hidden lg:flex flex-col gap-5 rounded-md shadow-lg absolute top-0 bottom-0 right-10 m-auto bg-white p-6 h-fit w-1/3'>
+        <hi className="text-4xl font-bold text-indigo-900">Hi, I'm Serge</hi>
+        <p className='text-gray-400'>
+          With over 1 years of experience on web design  and developpement. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. N oumquam quo
+          provident, facere minus temporibus venian nostrum reprehenderit nihil ? 
+        </p>
+        <a className="bg-indigo-600 text-white text-xl px-3 py-2 rounded-md font-semibold w-fit" href="#contact">Hire Me</a>
+      </div>
     </div>
   );
 }
